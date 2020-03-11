d2afffb7ca816022d7b23f9303400a3aab33f9b4
Ryan North <patrickrnorth@gmail.com>
2020-03-11 18:59:34 -0400

    Added readme and files

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+
+    public GameObject movingClouds;
+    public float cloudSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingClouds.transform.Translate(Vector3.forward * cloudSpeed * Time.deltaTime);
+    }
+}
